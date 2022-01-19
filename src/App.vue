ff7d4c864d2a13843523541b96d2d70fa8e0ac70
Janik Tilgner <61252825+Janik0110@users.noreply.github.com>
2022-01-19 19:58:31 +0100

    chore: initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { LineChart, useLineChart } from 'vue-chart-3';
+
+const {lineChartProps} = useLineChart({
+  chartData: {
+    labels:[1,2,3],
+    datasets: [
+      {
+        label: 'My First dataset',
+        backgroundColor: '#f87979',
+        data: [10, 20, 30]
+      }
+    ]
+  }
+})
+</script>
+
+<template>
+  <LineChart v-bind="lineChartProps" />
+</template>
+
